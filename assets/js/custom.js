91ae7d16b5f7c83aaa5665a91bb375b200be6187
unknown <13arturius91@gmail.com>
2017-02-07 02:15:20 +0300

    check for query string on url

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,12 +7,17 @@ $(function(){
     $('.button-collapse').sideNav({'edge': 'left'});
 
     var url = window.location.href;
-    var params = url.split('?')[1].split('&');
-    for(var i =0;i<params.length;i++){
-        var temp = params[i].split('=');
-        var key   = temp[0];
-        var value = temp[1];
-        console.log(key +':'+value);
+
+    if(url.indexOf("?") > -1) {
+        var params = url.split('?')[1].split('&');
+        for(var i =0;i<params.length;i++){
+            var temp = params[i].split('=');
+            var key   = temp[0];
+            var value = temp[1];
+            console.log(key +':'+value);
+        }
+    }else{
+        $('#allp').addClass('active indigo lighten-1');
     }
 
     if(value == 'Northern'){
@@ -23,7 +28,5 @@ $(function(){
         $('#est').addClass('active indigo lighten-1');
     }else if(value == 'Western'){
         $('#wst').addClass('active indigo lighten-1');
-    }else{
-        $('#allp').addClass('active indigo lighten-1');
     }
 });
