ccce0ca3510e505339e23855d1d617b4adf8b7b8
SimonMuraya <13arturius91@gmail.com>
2017-03-06 14:21:53 +0300

    added a new filter parameter

--- application/controllers/Explore.php
+++ application/controllers/Explore.php
@@ -9,13 +9,16 @@ class Explore extends CI_Controller {
         $this->load->helper('url_helper');
     }
 
-    public function index($parameter1, $parameter2)
+    public function index($parameter1, $parameter2, $parameter3)
     {
 
-        if(isset($parameter1) && isset($parameter2)){
+        if(isset($parameter1) && isset($parameter2) && isset($parameter3)){
+
             $data['province'] = $parameter1;
 
             $data['season'] = $parameter2;
+
+            $data['functionality'] = $parameter3;
 
             $this->load->view('explore', $data);
         }else{
