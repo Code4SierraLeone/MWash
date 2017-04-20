5b01a1823ba3e84715d7431c4d25bb7c4c3cf875
SimonMuraya <13arturius91@gmail.com>
2017-04-20 11:43:01 +0300

    fixed url helper

--- application/controllers/Mwashbot.php
+++ application/controllers/Mwashbot.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mwashbot extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url_helper');
+        $this->load->library(array('session'));
+    }
 
     public function apiRequestWebhook($method, $parameters)
     {
